b0d6b2e336eec72d0a5daacd5c12d53d238e9a3d
Vir Patel <vdp5@wraylab01.genome.duke.edu>
2017-01-13 13:37:03 -0500

    update

--- scripts/fimo_parse_scoring.py
+++ scripts/fimo_parse_scoring.py
@@ -2,15 +2,20 @@ import os
 import csv
 import sys
 import argparse
+from Bio import SeqIO
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-memefile')
 parser.add_argument('-fimofile')
+parser.add_argument('-genbank')
+parser.add_argument('-genebank_name')
 args = parser.parse_args()
 args = vars(args)
 
 memefle = list(csv.reader(open(args['memefile']), delimiter='\t'))
 fimofle = list(csv.reader(open(args['fimofile']), delimiter='\t'))
+genbank =  args['genbank']
+gbname = args['genebank_name']
 totalmotifnum = 0
 motif2num = {}
 
@@ -31,6 +36,8 @@ for alpha in memefle:
         motif2num[str(counter)] = zeta
         counter += 1
 
+# use fimo gff file here
+
 for alpha in fimofle:
     if '##' in alpha[0]:
         continue
@@ -40,5 +47,16 @@ for alpha in fimofle:
     if pvalue < 10.0**(-8):
         gene2motifs.setdefault(genename, []).append(num)
 
-for alpha in gene2motifs:
-    print alpha
+
+output = open('../data/genbank/{}_proteins_by_protid.fasta'.format(gbname), 'w')
+
+for seq_record in SeqIO.parse(genbank, 'genbank'):
+    for zeta  in seq_record.features:
+        if zeta.type  == 'CDS':
+            if 'protein_id' in zeta.qualifiers:
+                output.write('> {}\n'.format(zeta.qualifiers['protein_id'][0]))
+                output.write('{}\n'.format(zeta.qualifiers['translation'][0]))
+
+
+
+
